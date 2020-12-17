77136893f3c881bb056c512afa654e26b8702449
Lina Sachuk <sachuklina@gmail.com>
2020-12-17 00:00:39 -0700

    add func

--- intro.js
+++ intro.js
@@ -60,6 +60,6 @@ function bigBrother() {
 
 // Before running this code, what do you think the output is?
 bigBrother(); //outputs: no me!
-
+bigBrother();
 
   // ===========================================================================
