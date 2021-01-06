7a7c9435e92c73921e2452f5a8ef2646731de9a6
Lina Sachuk <sachuklina@gmail.com>
2021-01-06 16:21:17 -0700

    add example

--- intro.js
+++ intro.js
@@ -144,3 +144,21 @@ console.log(superClone) // { a: 'a', b: 'b', c: { deep: 'try and copy me' } }
 var human = { mortal: true }
 var socrates = Object.create(human);
 human.isPrototypeOf(socrates); // true
+
+//Array.map() => to print '🗺'
+Array.prototype.map = function () { // what happens with arrow function here?
+    arr = [];
+    for (let i = 0; i < this.length; i++) {
+        arr.push((this[i] + '🗺'));
+    }
+    return arr;
+}
+console.log([1, 2, 3].map())
+
+//Date object => to have method .yesterday() which shows you yesterday's day in 'YYYY-MM-DD' format.
+Date.prototype.lastYear = function () {
+    return this.getFullYear() - 1;
+}
+
+new Date('1900-10-10').lastYear()
+  // don't worry if you didn't get this... we will expand on this later.
