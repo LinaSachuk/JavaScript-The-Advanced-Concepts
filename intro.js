32334b4c5727b1a4cc5e599993ee1e97a490dc94
Lina Sachuk <sachuklina@gmail.com>
2021-01-21 17:57:18 -0700

    add exercise: reverse a string

--- intro.js
+++ intro.js
@@ -181,21 +181,74 @@ const peter = createElf('Peter', 'fire');
 sam.atack()
 
 
-// Amazon shopping
-const user = {
-    name: 'Kim',
-    active: true,
-    cart: [],
-    purchases: []
+// Implementing an Array
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+    get(index) {
+        return this.data[index];
+    }
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.data;
+    }
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+    deleteAtIndex(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    }
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        console.log(this.data[this.length - 1]);
+        delete this.data[this.length - 1];
+        this.length--;
+    }
 }
 
+const myArray = new MyArray();
+myArray.push('hi');
+myArray.push('you');
+myArray.push('!');
+myArray.pop();
+myArray.deleteAtIndex(0);
+myArray.push('are');
+myArray.push('nice');
+myArray.shiftItems(0);
+console.log(myArray);
 
-  //Implement a cart feature:
-  // 1. Add items to cart.
-  // 2. Add 3% tax to item in cart
-  // 3. Buy item: cart --> purchases
-  // 4. Empty cart
 
-  //Bonus:
-  // accept refunds.
-  // Track user history.
+// Reverse a String
+function reverse(str) {
+    // check input
+    if (!str || typeof str != 'string' || str.length < 2) return str;
+
+    const backwards = [];
+    const totalItems = str.length - 1;
+    for (let i = totalItems; i >= 0; i--) {
+        backwards.push(str[i]);
+    }
+    return backwards.join('');
+}
+
+function reverse2(str) {
+    //check for valid input
+    return str.split('').reverse().join('');
+}
+
+const reverse3 = str => [...str].reverse().join('');
+
+reverse('Timbits Hi')
+reverse('Timbits Hi')
+reverse3('Timbits Hi')
+
